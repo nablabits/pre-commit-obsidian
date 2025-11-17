ccee6d5f15901ae462ffec17b125862cb428e821
David Reguera <birrick@gmail.com>
2025-11-17 20:41:02 +0100

    Evaluate timestamps using sed

--- sandbox/create-sandbox.sh
+++ sandbox/create-sandbox.sh
@@ -6,6 +6,9 @@ cd sandbox
 cp sample_new_file.md new_file.md
 
 # add some change to `existing_file.md` so it will be picked by the script
+# TODO: this probably will need an iterator over files.
 echo "Some change" >> existing_file.md
+echo "Some change" >> no_front_matter_file.md
+echo "Some change" >> file_without_time_stamp.md
 
-git add .
+git add *.md
