f4c44e98fd12be7a3f5eb41831d9bbc20cd0a49f
David Reguera <birrick@gmail.com>
2025-11-19 20:51:39 +0100

    Add a space in the filename

--- sandbox/create-sandbox.sh
+++ sandbox/create-sandbox.sh
@@ -8,7 +8,7 @@ cp sample_new_file.md new_file.md
 # add some change to `existing_file.md` so it will be picked by the script
 # TODO: this probably will need an iterator over files.
 echo "Some change" >> existing_file.md
-echo "Some change" >> no_front_matter_file.md
+echo "Some change" >> "no_front_matter file.md"
 echo "Some change" >> file_without_time_stamp.md
 
 git add *.md
--- sandbox/no_front_matter file.md
+++ sandbox/no_front_matter file.md
@@ -2,3 +2,5 @@
 
 This file does not have frontmatter, so the script should add it along with
 the timestamps.
+
+It should also be resilient to files with spaces in their names.
